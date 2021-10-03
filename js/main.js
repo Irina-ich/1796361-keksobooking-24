42f3e49b72a9cbff93fbe28451afdfeb6fba759d
Irina <akva7575@mail.ru>
2021-10-03 23:46:31 +0400

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,152 +1,72 @@
-// Функция взята из интернета и доработана
-// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+import { getRandomPositiveFloat } from './utils/get-random-positive-float.js';
+import { getRandomPositiveInteger } from './utils/get-random-positive-integer.js';
 
-function getRandomPositiveFloat (numberMin, numberMax, digits = 1) {
-  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
-  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
-  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max
-  const lower = Math.min(Math.abs(numberMin), Math.abs(numberMax));
-  const upper = Math.max(Math.abs(numberMin), Math.abs(numberMax));
-  // Обратите внимание, чтобы учесть условие, что диапазон может быть [0, ∞),
-  // мы не ругаем пользователя за переданное отрицательное число,
-  // а просто берём его по модулю с помощью Math.abs
-
-  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
-  // которое домножаем на разницу между переданными числами - это будет наша случайная дельта.
-  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
-  const result = Math.random() * (upper - lower) + lower;
-
-  // И в конце с помощью метода toFixed любого числа в JavaScript
-  // указать требуемое количество знаков после точки
-  return result.toFixed(digits);
-}
-getRandomPositiveFloat();
-// Функция взята из интернета и доработана
-// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
-
-function getRandomPositiveInteger (numberMin,numberMax) {
-  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
-  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
-  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max.
-
-  // После нам нужно убедиться, что пользователь не передал дробные значения,
-  // для этого на всякий пожарный случай нижнюю границу диапазона
-  // мы округляем к ближайшему большему целому с помощью Math.ceil,
-  // а верхнюю границу - к ближайшему меньшему целому с помощью Math.floor
-  const lower = Math.ceil(Math.min(Math.abs(numberMin), Math.abs(numberMax)));
-  const upper = Math.floor(Math.max(Math.abs(numberMin), Math.abs(numberMax)));
-  // Обратите внимание, чтобы учесть условие, что диапазон может быть [0, ∞),
-  // мы не ругаем пользователя за переданное отрицательное число,
-  // а просто берём его по модулю с помощью Math.abs
-
-  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
-  // которое домножаем на разницу между переданными числами плюс единица - это будет наша случайная дельта.
-  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
-  const result = Math.random() * (upper - lower + 1) + lower;
-  // "Плюс единица", чтобы включить верхнюю границу диапазона в случайные числа
-
-  // И в конце с помощью метода Math.floor мы округляем полученный результат,
-  // потому что Math.random() генерирует только дробные числа и ноль.
-  return Math.floor(result);
-}
-getRandomPositiveInteger();
-
-const AUTHOR =[
-  'avatar'
+// const OFFERS_COUNT = 10;чтобы пропустил eslint
+const TITLES = [
+  'Квартира',
+  'Дом',
+  'Пентхаус',
+  'Комната',
+  'Аппартаменты',
 ];
-const OFFER=[
-  'title',
-  'address',
-  'price',
-  'type',
-  'rooms',
-  'guests',
-  'checkin',
-  'checkout',
-  'features',
-  'description',
-  'photos'
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
 ];
-const LOCATION=[
-  'lat',
-  'lng',
+const CHECKINS = ['12:00', '13:00', '14:00'];
+const CHECKOUTS = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DESCRIPTIONS = [
+  'Уютное гнездышко',
+  'Великолепный дом',
+  'Современное жилье',
+  'Бюджетный вариант',
+  'С красивым видом из окна',
 ];
-const SIMILAR_NOTICE_COUNT = 3;
-const createNotice = () => {
-  const randomAvatarImage = _.random(0, AUTHOR.length - 1);
-  const randomPrice=_.random(0, 1000000);
-  const randomType=_.sample(['palace', 'flat','house','bungalow','hotel']);
-  const randomRooms=_.random(0, 10);
-  const randomGuests=_.random(0, 50);
-  const randomCheckin=_.sample(['12:00', '13:00', '14:00']);
-  const randomCheckout=_.sample(['12:00', '13:00', '14:00']);
-  const features =[wifi, dishwasher, parking, washer, elevator, conditioner]
-    function getArray(features) {
-      const maxLength = features.length;
-      const lengthOfArray = getRandomNumber(1, maxLength);
-      const array = [];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
 
-      while (array.length < lengthOfArray) {
-        const indexOfEl = getRandomNumber(0, maxLength - 1);
-        const el = features[indexOfEl];
+const counter = (initial) => () => initial++;
+const avatarCounter = counter(1);
 
-        if (!array.includes(el)) {
-          array.push(el);
-        }
-      }
-      return array;
-
-      function getRandomNumber(from, to) {
-        return Math.floor(Math.random() * (to - from + 1)) + from;
-      }
-    };
-  const photos=['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg']
-    function getArray(photos) {
-      const maxLength = photos.length;
-      const lengthOfArray = getRandomNumber(1, maxLength);
-      const array = [];
-
-      while (array.length < lengthOfArray) {
-        const indexOfEl = getRandomNumber(0, maxLength - 1);
-        const el = photos[indexOfEl];
-
-        if (!array.includes(el)) {
-          array.push(el);
-        }
-      }
-      return array;
-
-      function getRandomNumber(from, to) {
-        return Math.floor(Math.random() * (to - from + 1)) + from;
-      }
-    };
-  const randomLat=_.random(35.65000,35.70000);
-  const randomLng=_.random(139.70000,139.80000);
-    return {
-      avatar:'img/avatars/user'+'0'+ AUTHOR[randomAvatarImage]+'png',
-      offer:OFFER[
-        {title:'Милая, уютная квартирка в центре Токио'},
-        {address:'{{location.lat}}, {{location.lng}}'},
-        {price:randomPrice},
-        {type:'randomType'},
-        {rooms:randomRooms},
-        {guests:randomGuests},
-        {checkin:'randomCheckin'},
-        {checkout:'randomCheckout'},
-        {features:getArray(features)},
-        {description:'Просторное,светлое,уютное помещение'},
-        {photos:getArray(photos)}
-      ],
-      location:LOCATION[
-        {lat: randomLat},
-        {lng:randomLng},
-      ]
-    };
+const getAvatar = () => {
+  const count = avatarCounter();
+  return `img/avatars/user${count < 10 ? `0${count}` : count}.png`;
 };
-const similarNotice = Array.from({length: SIMILAR_NOTICE_COUNT}, createNotice);
-console.log(
-  similarNotice);
 
+const createNotice = () => {
+  const lat = getRandomPositiveFloat(35.65000, 35.70000, 5);
+  const lng = getRandomPositiveFloat(139.70000, 139.80000, 5);
 
+  return {
+    author: {
+      avatar: getAvatar(),
+    },
+    offer: {
+      title: TITLES[getRandomPositiveInteger(0, TITLES.length - 1)],
+      address: `${lat}, ${lng}`,
+      price: getRandomPositiveInteger(1, 1000000),
+      type: TYPES[getRandomPositiveInteger(0, TYPES.length - 1)],
+      rooms: getRandomPositiveInteger(1, 10),
+      guests: getRandomPositiveInteger(1, 50),
+      checkin: CHECKINS[getRandomPositiveInteger(0, CHECKINS.length - 1)],
+      checkout: CHECKOUTS[getRandomPositiveInteger(0, CHECKOUTS.length - 1)],
+      features: FEATURES.slice(getRandomPositiveInteger(0, FEATURES.length - 1)),
+      description: DESCRIPTIONS[getRandomPositiveInteger(0, DESCRIPTIONS.length - 1)],
+      photos: PHOTOS.slice(getRandomPositiveInteger(0, PHOTOS.length - 1)),
+    },
+    location: {
+      lat: Number(lat),
+      lng: Number(lng),
+    },
+  };
+};
+
+// const similarNotice = Array.from({length: OFFERS_COUNT}, createNotice);чтобы пропустил eslint
+createNotice();
