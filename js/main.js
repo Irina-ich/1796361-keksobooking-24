48f6d26251991824a3ed615ac7da6b1462c8c706
Irina <akva7575@mail.ru>
2021-10-14 00:40:09 +0400

    l

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 import {createNotice} from './data.js';
 import {OFFERS_COUNT} from './consts.js';
+import {createCard} from './cards.js';
 
-new Array(OFFERS_COUNT).fill(null).map(() => createNotice());
-createNotice();
+const offers = new Array(OFFERS_COUNT).fill(null).map(() => createNotice());  //[{},{}]
+
+const container = document.querySelector('#map-canvas');
+
+offers.forEach((offer) => {
+  container.appendChild(createCard(offer));
+});
